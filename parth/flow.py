12e03dcf8a96999d8f1bcc583fe5dfe226ffa41d
Parth Kulshreshtha <kulshreshthaparth@gmail.com>
2025-04-06 11:17:24 +0530

    tried to add a new feature

--- parth/flow.py
+++ parth/flow.py
@@ -2,7 +2,7 @@ from util.chunking import chunk_document
 from util.data_ingestion import clean_rfp_text, extract_text_from_pdf
 from util.logger_config import get_logger
 
-RFP_PATH = "/home/kparth/HomersHackers/data/ELIGIBLE_RFP_2.pdf"
+RFP_PATH = "/home/kparth/HomersHackers/parth/data/ELIGIBLE_RFP_2.pdf"
 
 logger = get_logger("rfp_pipeline")
 
@@ -22,14 +22,14 @@ def execute():
 
     logger.info("Chunking the cleaned document.")
     chunks = chunk_document(document)
-    # return chunks
-    logger.info(f"Document split into {len(chunks)} chunks.")
+    return chunks
+#     logger.info(f"Document split into {len(chunks)} chunks.")
 
-    for i, chunk in enumerate(chunks):
-        logger.debug(f"Printing chunk {i+1}")
-        print(f"\n\033[96mChunk {i+1}:\033[0m\n{chunk}\n")
+#     for i, chunk in enumerate(chunks):
+#         logger.debug(f"Printing chunk {i+1}")
+#         print(f"\n\033[96mChunk {i+1}:\033[0m\n{chunk}\n")
 
-    logger.info("RFP processing pipeline complete.")
+#     logger.info("RFP processing pipeline complete.")
 
 
-print(execute())
+# print(execute())
